34a373763819cc95dd9915e8e017861254f493b5
Stephen Wright <wrightsah@icloud.com>
2021-02-08 16:33:59 -0500

    README generation works

--- script/index.js
+++ script/index.js
@@ -8,70 +8,106 @@ const inquirer = require("inquirer");
 
 // Questions
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "username",
-    message: "What is your Github username?",
-  },
-  {
-    type: "input",
-    name: "emailAddress",
-    message: "What is your email address?",
-  },
-  {
-    type: "input",
-    name: "title",
-    message: "What is the name of your project?",
-  },
-  {
-    type: "input",
-    name: "descripton",
-    message: "How would you describe your project?",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What is necessary for installation?",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What command should be run to install dependencies?",
-  },
-  {
-    type: "checkbox",
-    name: "licenseInput",
-    message: "What type of license would you like to use?",
-    choices: [],
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "How to contribute to the project?",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "What command should be run to run tests?",
-  },
-  {},
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your Github username?",
+    },
+    {
+      type: "input",
+      name: "emailAddress",
+      message: "What is your email address?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "descripton",
+      message: "How would you describe your project?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What is necessary for installation?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What type of license would you like to use?",
+      choices: [
+        "Apache License 2.0",
+        "GNU General Public License",
+        "MIT License",
+      ],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "How to contribute to the project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command should be run to run tests?",
+    },
+  ])
+  .then((data) =>
+    // console.log(response)
+    fs.appendFile(
+      "README.md",
 
-// README Contents
+      // README Contents
 
-// Description
+      `# ${data.title}
 
-// Table of Contents
+      ## Description ${data.descripton}
+      
+      ## Table of Contents
+      * [Installation](#Installation)
+      * [Usage](#Usage)
+      * [License](#License)
+      * [Contributing](#Contributing)
+      * [Tests](#Tests)
+      * [Questions](#Questions)
+      
+      ## Installation
+      Run the following command to install dependecies:
+      \`\`\`
+      ${data.installation}
+      \`\`\`
 
-// Installation
+      ## Usage
+      ${data.usage}
 
-// Usage
+      ## License
+      This project uses the following license: ${data.license}
 
-// License
+      ## Contributing
+      ${data.contributing}
+      
+      ## Tests
+      Run the following command to run tests: 
+      \`\`\`
+      ${data.tests}
+      \`\`\`
+      
+      ## Questions
+      If you have any questions, you may contace me at ${data.email}. 
+    
+      `,
 
-// Contributing
-
-// Tests
-
-// Questions
+      (error) => {
+        error ? console.log(error) : console.log("README creation successful!");
+      }
+    )
+  );
